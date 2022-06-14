9ebfb2d5ebe22747a0cd5a4b56bad2a9a88062dc
Stephen Chung <schungx@live.com>
2022-06-14 11:23:59 +0800

    Add rand ranges.

--- src/lib.rs
+++ src/lib.rs
@@ -73,7 +73,7 @@
 //! |  Feature   | Default  | Description                                                                        |
 //! | :--------: | :------: | ---------------------------------------------------------------------------------- |
 //! | `metadata` | disabled | includes functions metadata: parameter names/types, return type, doc-comments      |
-//! | `decimal`  | Disabled | provides random [decimal](https://crates.io/crates/rust_decimal) number generation |
+//! | `decimal`  | disabled | provides random [decimal](https://crates.io/crates/rust_decimal) number generation |
 //! |  `float`   | enabled  | provides random floating-point number generation                                   |
 //! |  `array`   | enabled  | provides methods for [Rhai] arrays                                                 |
 //!
@@ -82,22 +82,26 @@
 //!
 //! The following functions are defined in this package:
 //!
-//! |      Function       | Return type | Feature | Description                                                                  |
-//! | :-----------------: | :---------: | :-----: | ---------------------------------------------------------------------------- |
-//! |      `rand()`       |    `INT`    |         | generates a random number                                                    |
-//! | `rand(start..end)`  |    `INT`    |         | generates a random number within the exclusive range `start..end`            |
-//! | `rand(start..=end)` |    `INT`    |         | generates a random number within the inclusive range `start..=end`           |
-//! |   `rand_float()`    |   `FLOAT`   | `float` | generates a random floating-point number between `0.0` and `1.0` (exclusive) |
-//! |  `rand_decimal()`   |  `Decimal`  |`decimal`| generates a random [decimal](https://crates.io/crates/rust_decimal) number   |
-//! |    `rand_bool()`    |   `bool`    |         | generates a random boolean                                                   |
-//! |   `rand_bool(p)`    |   `bool`    | `float` | generates a random boolean with the probability `p` of being `true`          |
-//! | `Array::shuffle()`  |             | `array` | shuffles the elements in the [Rhai] array                                    |
-//! | `Array::sample()`   |  `Dynamic`  | `array` | copies a random element from the [Rhai] array                                |
-//! | `Array::sample(n)`  |   `Array`   | `array` | copies a non-repeating random sample of elements from the [Rhai] array       |
+//! |          Function          | Return value |  Feature  | Description                                                            |
+//! | :------------------------: | :----------: | :-------: | ---------------------------------------------------------------------- |
+//! |          `rand()`          |    `INT`     |           | generates a random integer number                                      |
+//! |     `rand(start..end)`     |    `INT`     |           | generates a random integer number within an exclusive range            |
+//! |    `rand(start..=end)`     |    `INT`     |           | generates a random integer number within an inclusive range            |
+//! |     `rand(start, end)`     |    `INT`     |           | generates a random integer number within a range                       |
+//! |       `rand_float()`       |   `FLOAT`    |  `float`  | generates a random floating-point number between `0.0` and `1.0`       |
+//! |  `rand_float(start, end)`  |   `FLOAT`    |  `float`  | generates a random floating-point number an exclusive range            |
+//! |      `rand_decimal()`      | [`Decimal`]  | `decimal` | generates a random [decimal][`Decimal`] number                         |
+//! | `rand_decimal(start, end)` | [`Decimal`]  | `decimal` | generates a random [decimal][`Decimal`] number within a range          |
+//! |       `rand_bool()`        |    `bool`    |           | generates a random boolean                                             |
+//! |       `rand_bool(p)`       |    `bool`    |           | generates a random boolean with the probability `p` of being `true`    |
+//! |     `Array.shuffle()`      |              |  `array`  | shuffles the items in the [Rhai] array                                 |
+//! |      `Array.sample()`      |  `Dynamic`   |  `array`  | copies a random element from the [Rhai] array                          |
+//! |     `Array.sample(n)`      |   `Array`    |  `array`  | copies a non-repeating random sample of elements from the [Rhai] array |
 //!
 //!
 //! [Rhai]: https://rhai.rs
 //! [`rand`]: https://crates.io/crates/rand
+//! [`Decimal`]: https://crates.io/crates/rust_decimal
 
 use rand::prelude::*;
 use rhai::def_package;
@@ -225,6 +229,28 @@ mod rand_functions {
         }
     }
 
+    /// Generate a random integer number within an inclusive range.
+    ///
+    /// # Example
+    ///
+    /// ```rhai
+    /// let number = rand(18, 38);
+    ///
+    /// print(`I'll give you a random number between 18 and 38: ${number}`);
+    /// ```
+    #[rhai_fn(name = "rand", return_raw)]
+    pub fn rand_from_to_inclusive(start: INT, end: INT) -> Result<INT, Box<EvalAltResult>> {
+        if start >= end {
+            Err(EvalAltResult::ErrorArithmetic(
+                format!("Range is empty: {}..{}", start, end),
+                Position::NONE,
+            )
+            .into())
+        } else {
+            Ok(rand::thread_rng().gen_range(start..=end))
+        }
+    }
+
     /// Generate a random floating-point number between `0.0` and `1.0` (exclusive).
     ///
     /// `1.0` is _excluded_ from the possibilities.
@@ -240,22 +266,66 @@ mod rand_functions {
     pub fn rand_float() -> FLOAT {
         rand::random()
     }
-
-    /// Generate a random [decimal](https://crates.io/crates/rust_decimal) number
-    /// between `0.0` and `1.0` (exclusive).
+    /// Generate a random floating-point number within an exclusive range.
     ///
-    /// `1.0` is _excluded_ from the possibilities.
+    /// # Example
+    ///
+    /// ```rhai
+    /// let number = rand_float(123.456, 789.678);
+    ///
+    /// print(`I'll give you a random number between 123.456 and 789.678: ${number}`);
+    /// ```
+    #[cfg(feature = "float")]
+    #[rhai_fn(name = "rand_float", return_raw)]
+    pub fn rand_float_range(start: FLOAT, end: FLOAT) -> Result<FLOAT, Box<EvalAltResult>> {
+        if start >= end {
+            Err(EvalAltResult::ErrorArithmetic(
+                format!("Range is empty: {}..{}", start, end),
+                Position::NONE,
+            )
+            .into())
+        } else {
+            Ok(rand::thread_rng().gen_range(start..=end))
+        }
+    }
+
+    /// Generate a random [decimal](https://crates.io/crates/rust_decimal) number.
     ///
     /// # Example
     ///
     /// ```rhai
     /// let number = rand_decimal();
     ///
-    /// print(`I'll give you a random number between 0 and 1: ${number}`);
+    /// print(`I'll give you a random decimal number: ${number}`);
     /// ```
     #[cfg(feature = "decimal")]
     pub fn rand_decimal() -> rust_decimal::Decimal {
         rand::random()
+    }
+    /// Generate a random [decimal](https://crates.io/crates/rust_decimal) number within a range.
+    ///
+    /// # Example
+    ///
+    /// ```rhai
+    /// let number = rand(18.to_decimal(), 38.to_decimal());
+    ///
+    /// print(`I'll give you a random number between 18 and 38: ${number}`);
+    /// ```
+    #[cfg(feature = "decimal")]
+    #[rhai_fn(name = "rand_decimal", return_raw)]
+    pub fn rand_decimal_range(
+        start: rust_decimal::Decimal,
+        end: rust_decimal::Decimal,
+    ) -> Result<rust_decimal::Decimal, Box<EvalAltResult>> {
+        if start >= end {
+            Err(EvalAltResult::ErrorArithmetic(
+                format!("Range is empty: {}..{}", start, end),
+                Position::NONE,
+            )
+            .into())
+        } else {
+            Ok(rand::thread_rng().gen_range(start..=end))
+        }
     }
 }
 
